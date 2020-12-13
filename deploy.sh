fc45ffb830a1c36d3d91c4b7520bcbe9dfcce0fc
invierno <1354002980@qq.com>
2020-12-13 20:47:36 +0800

    updated1

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@ git checkout gh-pages &&
 rm -rf images &&
 rm -rf *.html *.js *.css &&
 mv dist/* ./ &&
+rm -rf dist ;
 git add . &&
-git commit -m "updated" &&
+git commit -m "updated1" &&
 git push &&
 git checkout -
